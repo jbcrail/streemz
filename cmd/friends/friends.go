36b7abb972c22c9da422e272f666e1566ec8d1e0
Joseph Crail <jbcrail@gmail.com>
2017-05-20 02:22:30 -0400

    Update friends subcommand

--- cmd/friends/friends.go
+++ cmd/friends/friends.go
@@ -1,58 +1,40 @@
 package friends
 
 import (
+	"flag"
+
 	"github.com/jbcrail/streemz/cmdutil"
 
 	"github.com/dghubble/go-twitter/twitter"
 )
 
-func myFriends(client *twitter.Client) {
-	cursor := int64(-1)
-	for {
-		friends, resp, _ := client.Friends.List(&twitter.FriendListParams{
-			Cursor: cursor,
-		})
-
-		if cmdutil.IsRateLimitExceeded(resp) {
-			break
-		}
-
-		for _, user := range friends.Users {
-			cmdutil.PrintUserSummary(&user)
-		}
-		if friends.NextCursor == 0 {
-			break
-		}
-		cursor = friends.NextCursor
-	}
-}
-
-func friends(client *twitter.Client, name string) {
-	cursor := int64(-1)
-	for {
-		friends, resp, _ := client.Friends.List(&twitter.FriendListParams{
-			ScreenName: name,
-			Cursor:     cursor,
-		})
-
-		if cmdutil.IsRateLimitExceeded(resp) {
-			break
-		}
-
-		for _, user := range friends.Users {
-			cmdutil.PrintUserSummary(&user)
-		}
-		if friends.NextCursor == 0 {
-			break
-		}
-		cursor = friends.NextCursor
-	}
-}
-
 func Run(client *twitter.Client, args []string) {
-	if len(args) == 0 {
-		myFriends(client)
-	} else {
-		friends(client, args[0])
+	cmd := flag.NewFlagSet("friends", flag.ExitOnError)
+
+	cmd.Parse(args)
+
+	params := twitter.FriendListParams{
+		Cursor: int64(-1),
+	}
+
+	if cmd.NArg() > 0 {
+		params.ScreenName = cmd.Arg(0)
+	}
+
+	for {
+		friends, resp, _ := client.Friends.List(&params)
+
+		if cmdutil.IsRateLimitExceeded(resp) {
+			break
+		}
+
+		for _, user := range friends.Users {
+			cmdutil.PrintUserSummary(&user)
+		}
+
+		if friends.NextCursor == 0 {
+			break
+		}
+		params.Cursor = friends.NextCursor
 	}
 }
