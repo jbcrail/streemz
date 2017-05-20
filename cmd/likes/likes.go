5545f1566340f35651ea65c5ed88dcdd782364e2
Joseph Crail <jbcrail@gmail.com>
2017-05-20 02:02:53 -0400

    Refactor likes command to use flags

--- cmd/likes/likes.go
+++ cmd/likes/likes.go
@@ -1,41 +1,39 @@
 package likes
 
 import (
+	"flag"
+
 	"github.com/jbcrail/streemz/cmdutil"
 
 	"github.com/dghubble/go-twitter/twitter"
 )
 
-func myLikes(client *twitter.Client) {
-	tweets, resp, _ := client.Favorites.List(&twitter.FavoriteListParams{})
-
-	if cmdutil.IsRateLimitExceeded(resp) {
-		return
-	}
-
-	for _, tweet := range tweets {
-		cmdutil.PrintTweet(tweet)
-	}
-}
-
-func likes(client *twitter.Client, name string) {
-	tweets, resp, _ := client.Favorites.List(&twitter.FavoriteListParams{
-		ScreenName: name,
-	})
-
-	if cmdutil.IsRateLimitExceeded(resp) {
-		return
-	}
-
-	for _, tweet := range tweets {
-		cmdutil.PrintTweet(tweet)
-	}
-}
-
 func Run(client *twitter.Client, args []string) {
-	if len(args) == 0 {
-		myLikes(client)
-	} else {
-		likes(client, args[0])
+	cmd := flag.NewFlagSet("likes", flag.ExitOnError)
+	count := cmd.Int("count", 20, "")
+	full := cmd.Bool("full", false, "")
+
+	cmd.Parse(args)
+
+	params := twitter.FavoriteListParams{
+		Count: *count,
+	}
+
+	if cmd.NArg() > 0 {
+		params.ScreenName = cmd.Arg(0)
+	}
+
+	tweets, resp, _ := client.Favorites.List(&params)
+
+	if cmdutil.IsRateLimitExceeded(resp) {
+		return
+	}
+
+	for _, tweet := range tweets {
+		if *full {
+			cmdutil.PrintExtendedTweet(tweet)
+		} else {
+			cmdutil.PrintTweet(tweet)
+		}
 	}
 }
